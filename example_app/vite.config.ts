06062d9e2543efdbe994cd97ecf57e3c0907ce18
zNitche <53448802+zNitche@users.noreply.github.com>
2026-09-21 19:58:50 +0200

    feat(app): baseline setup

---
+++ example_app/vite.config.ts
@@ -0,0 +1,21 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import { resolve } from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        port: 8080,
+        host: true,
+    },
+    build: {
+        target: "es2023",
+        sourcemap: false,
+    },
+    resolve: {
+        alias: {
+            "@root": resolve(import.meta.dirname, "./src"),
+        },
+    },
+})
