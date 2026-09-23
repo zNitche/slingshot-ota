d6b7286ce70d1790a7f5ddb01bb055e70612f11e
zNitche <53448802+zNitche@users.noreply.github.com>
2026-09-23 21:34:45 +0200

    refactor(app): minor changes

--- example_app/src/App.tsx
+++ example_app/src/App.tsx
@@ -1,17 +1,18 @@
-import { useEffect } from "react";
 import classes from "./App.module.css";
-import { SlingshotUpdater } from "slingshot-updater";
+// import { SlingshotUpdater } from "slingshot-updater";
 
 export default function App() {
-    useEffect(() => {
-        const c = setInterval(() => { 
-            SlingshotUpdater.echo({ value: "slingshot ota echo" });
-        }, 1000)
+    // useEffect(() => {
+    //     const c = setInterval(() => { 
+    //         SlingshotUpdater.echo({ value: "slingshot ota echo" });
+    //     }, 1000)
 
-        return () => clearInterval(c);
-    })
+    //     return () => clearInterval(c);
+    // })
 
     return (
-        <div className={classes.app}>Hello world</div>
+        <div className={classes.app}>
+            <div className={classes.content}>Slingshot Updater Test App</div>
+        </div>
     )
 }
